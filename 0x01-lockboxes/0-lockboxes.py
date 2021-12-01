600512871758ab3445b7482d64ad1c4a7ca8f895
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 00:48:41 +0300

    open all the boxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+unlock all the boxes
+The first box is open
+and every box has a key to another box
+"""
+
+
+def openBoxes(unopenedBoxes, openedBoxes, boxes):
+    """
+    use all the keys to open the boxes as much as you can
+    """
+    if len(unopenedBoxes) == 0:
+        if len(openedBoxes) == len(boxes):
+            return True
+        else:
+            return False
+    box = unopenedBoxes.pop()
+    if box not in openedBoxes:
+        for key in boxes[box]:
+            if key not in openedBoxes and key < len(boxes):
+                unopenedBoxes.add(key)
+    openedBoxes.add(box)
+    return openBoxes(unopenedBoxes, openedBoxes, boxes)
+
+
+def canUnlockAll(boxes):
+    openedBoxes = set()
+    unopenedBoxes = set()
+    unopenedBoxes.add(0)
+    x = openBoxes(unopenedBoxes, openedBoxes, boxes)
+    return x
