0311bd20949fe09417042455714f8b17c9e279ca
amanabiy <amanuelsisay.as@gmail.com>
2021-12-02 01:17:41 +0300

    using list find all the keys

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -5,29 +5,15 @@ The first box is open
 and every box has a key to another box
 """
 
-
-def openBoxes(unopenedBoxes, openedBoxes, boxes):
-    """
-    use all the keys as much as you can using set
-    """
-    if len(unopenedBoxes) == 0:
-        if len(openedBoxes) == len(boxes):
-            return True
-        else:
-            return False
-    box = unopenedBoxes.pop()
-    if box not in openedBoxes:
-        for key in boxes[box]:
-            if key not in openedBoxes and key < len(boxes):
-                unopenedBoxes.add(key)
-    openedBoxes.add(box)
-    return openBoxes(unopenedBoxes, openedBoxes, boxes)
-
-
 def canUnlockAll(boxes):
-    """ using set """
-    openedBoxes = set()
-    unopenedBoxes = set()
-    unopenedBoxes.add(0)
-    x = openBoxes(unopenedBoxes, openedBoxes, boxes)
-    return x
+    """ find every key to open all """
+    size = len(boxes)
+    keys = [0]
+    for i in keys:
+        for key in boxes[i]:
+            if key not in keys and key < size:
+                keys.append(key)
+    if len(keys) == size:
+        return True
+    else:
+        return False
