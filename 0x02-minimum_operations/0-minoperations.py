a760d2313669749504907d16cfe86078162c0988
amanabiy <amanuelsisay.as@gmail.com>
2021-12-13 00:04:07 +0300

    handle very large numbers

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -2,45 +2,22 @@
 """
 In a text file, there is a single character H.
 Your text editor can execute only two operations in this file:
-Copy All and Paste. Given a number n, write a method
-that calculates the fewest number of operations needed to
+Copy All and Paste. Given a nber n, write a method
+that calculates the fewest nber of operations needed to
 result in exactly n H characters in the file.
 """
 
 
 def minOperations(n):
-    """ find the min operations needed to achive the required H number """
+    """ Return sum of process until n H """
     if n < 2 or not isinstance(n, int):
         return 0
-    letter = 1
-    copy = 1
-    op = 1
-    nim = 100000000000000
-    factors = []
-
-    # find all the dividors
-    for factor in range(2, (n // 2) + 1):
-        if n % factor == 0:
-            factors.append(factor)
-
-    nim = min(factors)  # find the min divider to use latter
-
-    # count the operation
-    while letter <= n:
-        if letter == n:
-            return op
-        # paste until reaching the least factor
-        if letter < nim:
-            op += 1
-            letter += copy
-        # paste
-        if letter in factors:
-            op += 1
-            copy = letter
-            op += 1
-            letter += copy
-            # print(f"copy-paste:  paste letter: {letter} copy: {copy}")
-        else:
-            op += 1
-            letter += copy
-    return 0
+    count = 1
+    final = []
+    while n != 1:
+        count += 1
+        if n % count == 0:
+            while (n % count == 0 and n != 1):
+                n /= count
+                final.append(count)
+    return sum(final)
